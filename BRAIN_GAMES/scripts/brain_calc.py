46de8d9fc26259d131a8dffe1afbf6ecba7020a8
juuliadidenko <hiljaotsola2766@gmail.com>
2022-06-12 21:34:42 +0300

    Add brain_calc.py

---
+++ BRAIN_GAMES/scripts/brain_calc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from BRAIN_GAMES.engine import start_game
+from BRAIN_GAMES.games import calc
+
+
+def main():
+    start_game(calc)
+
+
+if __name__ == '__main__':
+    main()
