6224eee4ea3ab6d9c183bd4c4fcc41096793ad15
Julia Didenko <iuliia.didenko@gmail.com>
2022-07-23 16:32:22 +0300

    change step range in progression.py

--- BRAIN_GAMES/games/progression.py
+++ BRAIN_GAMES/games/progression.py
@@ -6,11 +6,11 @@ progression_len = 10
 
 def generate_round_problem():
     gen_number = random.randint(0, 30)
-    step = random.randint(1, 10)
+    step = random.randint(1, 5)
     end = random.randint(40, 100)
     sequence = list(range(gen_number, end, step))
     answer = random.choice(sequence)
     question = ' '.join(
         '..' if number == answer else str(number) for number in sequence
     )
-    return question, answer
+    return question, str(answer)
